c90004077495591bfad0946d069549d78a5e3307
LucasMellogh <45610313+LucasMellogh@users.noreply.github.com>
2018-12-09 18:49:08 -0200

    Update agendamento.cpp

--- agendamento.cpp
+++ agendamento.cpp
@@ -13,9 +13,12 @@ Agendamento::Agendamento(string _data, PontoDeColeta &_local, Pessoa &_doador, P
     receptor = &_receptor;
     residuos = &_residuos;
     id = _id;
+    verficarData(_data, _id);
 }
 
-void Agendamento::setData(string _data) { data = _data;}
+void Agendamento::setData(string _data) {
+     verficarData(_data, id);
+    data = _data;}
 void Agendamento::setLocal(PontoDeColeta &_local) { local = &_local;}
 void Agendamento::setDoador(Pessoa &_doador) { doador = &_doador;}
 void Agendamento::setReceptor(Pessoa &_receptor) { receptor = &_receptor;}
@@ -29,3 +32,29 @@ Pessoa Agendamento::getDoador(){return *doador;}
 Pessoa Agendamento::getReceptor(){return *receptor;}
 Residuo Agendamento::getResiduos(){return *residuos;}
 string Agendamento::getId(){return id;}
+
+void Agendamento:: verficarData(string _data, string _nome){
+    string data = _data;
+    string mensagemErroData = "Erro: Data de " + _nome + " invalida.";
+
+    if(data.size() == 10 && ((data[2] == '/' && data[5] == '/') || (data[2] == '-' && data[5] == '-')))
+        {
+            string delimiter = "/";
+            string dia = data.substr(0, 2);
+            string mes = data.substr(3, 2);
+            string ano = data.substr(6, 4);
+
+            int _dia = atoi(dia.c_str());
+            int _mes = atoi(mes.c_str());
+            int _ano = atoi(ano.c_str());
+
+            if(_ano < 1900 || _ano > 2030 || _mes < 1 || _mes > 12 || _dia > 31 || _dia < 1)
+                throw mensagemErroData;
+            else if(_dia > 28 && _mes == 2)
+                throw mensagemErroData;
+            else if(_dia > 30 && (_mes == 4 || _mes == 6 || _mes == 9 || _mes == 11))
+                throw mensagemErroData;
+        }
+        else
+            throw mensagemErroData;
+}
